73878c04acc5e64d80083a6957fd17f4d75c69b1
Constantin Matei <coding@matei.uk>
2021-06-04 18:55:28 +0100

    feat: Show twitter feed of Hubble Space fix #1

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,9 @@
 import React from "react"
 import { render } from "react-dom"
+import TwitterSearch from "./component/twitter-search"
 
 const here = document.querySelector("#here")
+
+const twitterSearch = <TwitterSearch />
+
+render(twitterSearch, here)
