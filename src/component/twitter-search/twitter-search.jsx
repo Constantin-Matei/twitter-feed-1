ca16c67d85976231fcda81246e2bc1a0c72e159f
Constantin Matei <coding@matei.uk>
2021-06-06 17:03:10 +0100

    feat: Implement dynamic input so we can make the feed searchable fix #2

--- src/component/twitter-search/twitter-search.jsx
+++ src/component/twitter-search/twitter-search.jsx
@@ -1,18 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import { Timeline } from "react-twitter-widgets"
+import SmartForm from "../smart-form"
 
 const TwitterSearch = () => {
+  const [screenName, updateScreenName] = useState("hubble_space")
+
   return (
     <div>
       <Timeline
         dataSource={{
           sourceType: "profile",
-          screenName: "hubble_space",
+          screenName,
         }}
         options={{
           tweetLimit: "10",
         }}
       />
+      <SmartForm updateScreenName={updateScreenName} />
     </div>
   )
 }
